9c8b190830b33aa98f8d265f383830daa7e023d1
Anton <dr.dud34@gmail.com>
2021-10-04 23:05:38 +0300

    test build

--- museum/src/js/video.js
+++
@@ -1,8 +0,0 @@
-const progress = document.querySelectorAll('.progress');
-  
-progress.forEach( item => {
-    item.addEventListener('input', function() {
-        const value = this.value;
-        this.style.background = `linear-gradient(to right, #710707 ${value}%, #C4C4C4 ${value}%)`
-      })
-})
