4873c1e695537e24f1faa8b9e9073a441a6875d3
Anton <dr.dud34@gmail.com>
2021-12-08 23:17:48 +0300

    fix: fix api link

--- news-JS-main/src/components/controller/appLoader.ts
+++ news-JS-main/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '09a5744a5a034e69a7514ba1dd4afce5', // получите свой ключ https://newsapi.org/
         });
     }
